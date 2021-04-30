04542bb35d236208e4d6b174e33434d8698a0969
mokan <olympspartan@gmail.com>
2021-04-30 13:46:17 +0300

    fix relative path issue

--- lib/post_mdize.rb
+++ lib/post_mdize.rb
@@ -2,80 +2,84 @@
 
 require_relative "post_mdize/version"
 require 'json'
-require 'active_support/inflector/methods.rb'
+require 'active_support/inflector'
 require 'json_deep_parse'
+require 'pathname'
 
 module PostMdize
   class Error < StandardError; end
 
-  include ActiveSupport::Inflector
-  using ::JSONDeepParse
+  class Mdize
+    include ActiveSupport::Inflector
+    using ::JSONDeepParse
 
-  input_file  = ARGV.first
-  output_file = ARGV.last
+    def self.perform(input_file, output_file, dir_name)
+      pathname = Pathname.new(dir_name).relative_path_from(Pathname.new(`pwd`)).to_s
+      file = File.read(input_file)
+      hash = JSON.deep_parse(file)
 
-  file = File.read(input_file)
-  hash = JSON.deep_parse(file)
-
-  def path_mdize(item)
-    path_segs = item['request']['url']['path']
-    path_segs.map!.with_index do |segment, index|
-      segment.match?(/\-/) ? "{:#{singularize(path_segs[index-1])}_uuid}" : segment
-    end
-
-    "#{path_segs.join('/')}"
-  end
-
-  def body_mdize(item)
-    item['request']['body']['raw'] || ''
-  end
-
-  def response_mdize(item)
-    item['response'].first['body'] #unless item['response']['body'].empty?) || ''
-  end
-
-  def hash_mdize(hash, file, intense = 2)
-    hash = {} if hash == ""
-    space = ' ' * intense
-    if hash.is_a?(Array)
-      file.write("[\r\n")
-      hash.each do |element|
-        hash_mdize(element, file, intense + 2)
-      end
-      file.write("#{' ' * (intense - 2)}]")
-    else
-      file.write("#{' ' * (intense - 2)}{\r\n")
-      strings =
-      hash.map do |k, v|
-        if v.is_a?(Hash) || v.is_a?(Array)
-          file.write("#{space}\"#{k}\": ")
-          hash_mdize(v, file, intense + 2)
-        else
-          file.write("#{space}\"#{k}\": \"#{v}\"")
-          file.write(",\r\n") unless v == hash.values.last
-          #file.write("\r\n}\r\n")
+      outfile = File.open("#{pathname}/#{output_file}", "w") do |file|
+        file.write("# README\r\n")
+        hash['item'].each do |item|
+          file.write("\r\n* __#{item['request']['method']}__ #{path_mdize(item)}\r\n")
+          file.write("#### #{item['name']}\r\n")
+          file.write("params\r\n")
+          file.write("\r\n```\r\n")
+          file.write("header:\r\n")
+          file.write("#{item['header']}")
+          file.write("body:\r\n")
+          hash_mdize(body_mdize(item), file)
+          file.write("\r\n```\r\n")
+          file.write("RESPONSE:\r\n")
+          file.write("\r\n```\r\n")
+          hash_mdize(response_mdize(item), file)
+          file.write("\r\n```\r\n")
         end
+        puts "Generated:", file.path
       end
-      file.write("\r\n#{' ' * (intense - 2)}}\r\n")
-    end
-  end
 
-  outfile = File.open(output_file, "w") do |file|
-    file.write("# README\r\n")
-    hash['item'].each do |item|
-      file.write("\r\n* __#{item['request']['method']}__ #{path_mdize(item)}\r\n")
-      file.write("#### #{item['name']}\r\n")
-      file.write("params\r\n")
-      file.write("\r\n```\r\n")
-      file.write("header:\r\n")
-      file.write("#{item['header']}")
-      file.write("body:\r\n")
-      hash_mdize(body_mdize(item), file)
-      file.write("\r\n```\r\n")
-      file.write("RESPONSE:\r\n")
-      file.write("\r\n```\r\n")
-      hash_mdize(response_mdize(item), file)
-      file.write("\r\n```\r\n")
+    end
+
+    def self.path_mdize(item)
+      path_segs = item['request']['url']['path']
+      path_segs.map!.with_index do |segment, index|
+        segment.match?(/\-/) ? "{:#{self.new.singularize(path_segs[index-1])}_uuid}" : segment
+      end
+
+      "#{path_segs.join('/')}"
+    end
+
+    def self.body_mdize(item)
+      item['request']['body']['raw'] || ''
+    end
+
+    def self.response_mdize(item)
+      item['response'].first['body'] #unless item['response']['body'].empty?) || ''
+    end
+
+    def self.hash_mdize(hash, file, intense = 2)
+      hash = {} if hash == ""
+      space = ' ' * intense
+      if hash.is_a?(Array)
+        file.write("[\r\n")
+        hash.each do |element|
+          hash_mdize(element, file, intense + 2)
+        end
+        file.write("#{' ' * (intense - 2)}]")
+      else
+        file.write("#{' ' * (intense - 2)}{\r\n")
+        strings =
+        hash.map do |k, v|
+          if v.is_a?(Hash) || v.is_a?(Array)
+            file.write("#{space}\"#{k}\": ")
+            hash_mdize(v, file, intense + 2)
+          else
+            file.write("#{space}\"#{k}\": \"#{v}\"")
+            file.write(",\r\n") unless v == hash.values.last
+          end
+        end
+        file.write("\r\n#{' ' * (intense - 2)}}\r\n")
+      end
     end
   end
 end
--- post_mdize.gemspec
+++ post_mdize.gemspec
@@ -9,24 +9,25 @@ Gem::Specification.new do |spec|
   spec.email         = ["olympspartan@gmail.com"]
 
   spec.summary       = "SOME SUMMARY"
-  spec.description   = ""
-  spec.homepage      = 'https://github.com/KanielOutis/post_mdize'
+  spec.description   = "Gem for generating README MD documentation from postman collection documentation, from json to md"
+  spec.homepage      = 'https://rubygems.org/gems/post_mdize'
   spec.license       = "MIT"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.4.0")
 
   spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = ""
-  spec.metadata["changelog_uri"] = ""
+  spec.metadata["source_code_uri"] = spec.homepage
+  spec.metadata["changelog_uri"] = spec.homepage
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
   spec.files = Dir.chdir(File.expand_path(__dir__)) do
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{\A(?:test|spec|features)/}) }
   end
-  spec.bindir        = "exe"
+  spec.bindir        = "bin"
   spec.executables   = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
+  spec.executables  << 'post_mdize'
   spec.require_paths = ["lib"]
 
   # Uncomment to register a new dependency of your gem
